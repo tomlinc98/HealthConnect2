0ca3fc8aac131360d9c96c01f20c8707edfcd3de
Chris <tomlinc98@gmail.com>
2024-05-01 17:26:14 +0100

    Refactor authRoutes.ts to include user role in JWT generation

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -4,19 +4,20 @@ import jwt from "jsonwebtoken"
 
 const router = Router()
 
-// Helper function to generate JWT
-const generateToken = (id: string) => {
-  return jwt.sign({ _id: id }, process.env.JWT_SECRET!, { expiresIn: "24h" })
+// Helper function to generate JWT including user role
+const generateToken = (id: string, role: string) => {
+  return jwt.sign({ _id: id, role: role }, process.env.JWT_SECRET!, {
+    expiresIn: "24h",
+  })
 }
 
 // POST /auth/register
 router.post("/register", async (req, res) => {
-  const { email, password } = req.body // Remove role from here
-
+  const { email, password } = req.body
   try {
-    // Use the signup method from UserModel where the role is automatically set to patient
     const user = await User.signup(email, password)
-    const token = generateToken(user._id.toString())
+    const token = generateToken(user._id.toString(), user.role)
+
     res.status(201).json({ token, userId: user._id, role: user.role })
   } catch (error) {
     if (error instanceof Error) {
@@ -38,7 +39,7 @@ router.post("/login", async (req, res) => {
 
   try {
     const user = await User.login(email, password)
-    const token = generateToken(user._id.toString())
+    const token = generateToken(user._id.toString(), user.role)
     res.json({ token, userId: user._id, role: user.role })
   } catch (error: unknown) {
     if (error instanceof Error) {
